c9781107ce08d148e54c0efd64ce23760ac70cb8
Andrzej Marcinkowski <andrzej.marcinkowski@gmail.com>
2023-11-26 21:23:42 +0100

    u

--- login.sh
+++ login.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-Username=$1
-Password=$2
-GCPProjectID=$3
+#Username=$1
+#Password=$2
+#GCPProjectID=$3
 
-gcloud auth login $Username
-gcloud config set project $GCPProjectID
+alias ggcl="gcloud auth login $1"
+alias ggcp="gcloud config set project $1"
+alias ggce=`export PROJECT_ID=$(gcloud info --format='value(config.project)')`
 
 #gcloud config set compute/zone "ZONE"
 #export ZONE=$(gcloud config get compute/zone)
 #gcloud config set compute/region "REGION"
 #export REGION=$(gcloud config get compute/region)
 
-gcloud config set compute/zone us-east1-c
-gcloud config set compute/region us-east1
+#gcloud config set compute/zone us-east1-c
+#gcloud config set compute/region us-east1
 
-export PROJECT_ID=$(gcloud info --format='value(config.project)')
 
